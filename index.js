af9326eacb5889392a83543de21e2c3a11204a87
EvanBurnette <34970211+EvanBurnette@users.noreply.github.com>
2023-08-17 13:54:12 -0700

    Remove debug statements

--- index.js
+++ index.js
@@ -12,44 +12,39 @@ const classes = rawClasses
       .replace(/(\/|"|\\)/g, "")
   );
 
-// const noise_raw = await fetch("./noise.wav");
+// get sound file and get audio data
 const noise_raw = await fetch("./noise.wav");
 const audioCtx = new AudioContext();
 const noise = await audioCtx.decodeAudioData(await noise_raw.arrayBuffer());
-// console.log(noise);
+
+// get ML model
 const modelUrl = "https://tfhub.dev/google/tfjs-model/yamnet/tfjs/1";
 const model = await tf.loadGraphModel(modelUrl, { fromTFHub: true });
 const waveform = tf.tensor(noise.getChannelData(0));
 const [scores, embeddings, spectrogram] = model.predict(waveform);
-scores.print(true); // shape [N, 521]
-embeddings.print(true); // shape [N, 1024]
-// spectrogram.print(true); // shape [M, 64] // Find class with the top score when mean-aggregated across frames.
-console.log("class number: ", await scores.mean(0).argMax().array()); // Should print 494 corresponding to 'Silence' in YAMNet Class Map.
+// scores.print(true); // shape [N, 521]
+// embeddings.print(true); // shape [N, 1024]
+spectrogram.print(true); // shape [M, 64] // Find class with the top score when mean-aggregated across frames.
+// console.log("class number: ", await scores.mean(0).argMax().array()); // Should print 494 corresponding to 'Silence' in YAMNet Class Map.
 
+// get top 10 classes
 const top10 = await tf.topk(scores.mean(0), 10, true).indices.array();
-console.log("scores mean topk k=10", top10);
-
+// console.log("scores mean topk k=10", top10);
+// create one string from 10 classes
 let top10classes = "";
 for (const i of top10) {
   top10classes += "\n";
   top10classes += classes[i];
 }
-
+// add predictions to UI
 const predict_p = document.getElementById("predict");
 predict_p.innerText = top10classes;
-
-console.log("scores shape: ", scores.shape);
-scores.print();
-
-// const { values, indices } = tf.topk(scores.mean(0));
-// values.print();
-// indices.print();
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+console.log("minimum value :", await tf.min(spectrogram).array());
 console.log("maximum value :", await tf.max(spectrogram).array());
-// console.log(await spectrogram.array());
 
 const spectrogram_scaled = await tf
   .transpose(await normalize(spectrogram).NORMALIZED_VALUES, [1, 0])
@@ -57,7 +52,6 @@ const spectrogram_scaled = await tf
   .reverse(0);
 
 tf.browser.toPixels(spectrogram_scaled, canvas);
-// console.log(await spectrogram_scaled.array());
 
 function normalize(tensor, min, max) {
   const result = tf.tidy(function () {
